14e35f92c7da36a56816001f1c28836ef4eedc6e
ShivaniMudiga <shivanimudiga04@gmail.com>
2024-10-08 00:04:54 +0530

    Add files via upload
    Latest and updated code till overall analysis

---
+++ OVERALL_ANALYSIS/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Footer from "./components/Footer";
+import Home from './components/Home';
+import Game from './components/Game';
+import Analysis from './components/Analysis';
+import Login from './components/Login'; // Import Login component
+import OverallAnalysis  from './components/OverallAnalysis';
+function App() {
+  return (
+    <Router>
+      <div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} /> {/* New Login Route */}
+            <Route path="/game" element={<Game />} />
+            <Route path="/analysis" element={<Analysis />} />
+            <Route path="/Analysis/:sessionId" element={<OverallAnalysis/>}/>
+          </Routes>
+        </div>
+        {/* <Footer/> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
