8bc637657aac8d5c628af491f94ef8055c9df3fc
AnanyaGanji <ananyaganji06@gmail.com>
2024-10-24 19:13:00 +0530

    added timer bar

--- PROJECT/FRONTEND/src/components/Game.js
+++ PROJECT/FRONTEND/src/components/Game.js
@@ -1,14 +1,15 @@
 // import { Buffer } from 'buffer';
 // import process from 'process';
-import React, { useState, useEffect } from 'react';
-import { useRef } from 'react';
-import './Game.css';
-import confetti from 'canvas-confetti';
+import React, { useState, useEffect } from "react";
+import { useRef } from "react";
+import "./Game.css";
+import confetti from "canvas-confetti";
 // import Header from './Header';
-import axios from 'axios';
-import html2canvas from 'html2canvas'; // Import html2canvas
-import { useLocation } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid'; // Import uuid
+import axios from "axios";
+import html2canvas from "html2canvas"; // Import html2canvas
+import { useLocation } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid"; // Import uuid
+import TimerBar from "./TimerBar";
 
 const questions = [
   {
@@ -58,12 +59,12 @@ const questions = [
 
 const Game = () => {
   const location = useLocation();
-  const [sessionName, setSessionName] = useState('');
+  const [sessionName, setSessionName] = useState("");
 
   useEffect(() => {
-    const session = location.state?.sessionName || 'Unnamed Session';
+    const session = location.state?.sessionName || "Unnamed Session";
     setSessionName(session);
-    console.log('Session Name (in game.js):', session);  // Ensure this is printed correctly
+    console.log("Session Name (in game.js):", session); // Ensure this is printed correctly
   }, [location.state]);
 
   const [shuffledQuestions, setShuffledQuestions] = useState([]);
@@ -74,63 +75,62 @@ const Game = () => {
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [answerStates, setAnswerStates] = useState([]); // Array to track button states
   const [hasStarted, setHasStarted] = useState(false); // New state for game start
-  
+
   const [webcamGranted, setWebcamGranted] = useState(false); // State to track webcam access
   const [cameraActive, setCameraActive] = useState(false); // State to track if the camera is active
 
   const [sessionId, setSessionId] = useState(null); // State for session ID
-  const { v4: uuidv4 } = require('uuid');
-  let newSessionId="null";
+  const { v4: uuidv4 } = require("uuid");
+  let newSessionId = "null";
 
   const videoRef = useRef(null); // Reference to the video element
   const canvasRef = useRef(null); // Reference to the canvas element for capturing images
   const captureIntervalRef = useRef(null); // To store the interval ID for image capture
 
-
-  const [file, setFile] = useState(null); 
+  const [file, setFile] = useState(null);
   const isCameraActive = () => {
     const stream = videoRef.current?.srcObject;
     if (stream && stream.getVideoTracks().length > 0) {
       const track = stream.getVideoTracks()[0];
-      return track.readyState === 'live' && track.enabled; // Check if track is live and enabled
+      return track.readyState === "live" && track.enabled; // Check if track is live and enabled
     }
     return false;
   };
-  
+
   // Function to request webcam access
   const requestWebcamAccess = () => {
-    navigator.mediaDevices.getUserMedia({ video: true })
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
       .then((stream) => {
         videoRef.current.srcObject = stream;
         setWebcamGranted(true);
-        
-        const track = stream.getVideoTracks()[0];
-      
-      // Check if the camera track is live
-      if (track.readyState === 'live' && track.enabled) {
-        setCameraActive(true);
-      } else {
-        setCameraActive(false);
-      }
 
-        videoRef.current.style.display='none' // hide the vedio feed
+        const track = stream.getVideoTracks()[0];
+
+        // Check if the camera track is live
+        if (track.readyState === "live" && track.enabled) {
+          setCameraActive(true);
+        } else {
+          setCameraActive(false);
+        }
+
+        videoRef.current.style.display = "none"; // hide the vedio feed
 
         // Periodically check if the camera is still active
-      const checkInterval = setInterval(() => {
-        if (!isCameraActive()) {
-          //alert("Camera is not active. Please ensure the camera is turned on.");
-          setCameraActive(false);
-        } else {
-          console.log("Camera is active only ")
-          setCameraActive(true);
-        }
-      }, 5000); // Check every 5 seconds
+        const checkInterval = setInterval(() => {
+          if (!isCameraActive()) {
+            //alert("Camera is not active. Please ensure the camera is turned on.");
+            setCameraActive(false);
+          } else {
+            console.log("Camera is active only ");
+            setCameraActive(true);
+          }
+        }, 5000); // Check every 5 seconds
 
-      return () => clearInterval(checkInterval); // Clean up interval
-
+        return () => clearInterval(checkInterval); // Clean up interval
       })
       .catch((err) => {
-        console.error('Error accessing webcam:', err);
+        console.error("Error accessing webcam:", err);
         setWebcamGranted(false); // Webcam access denied
         setCameraActive(false); // Camera not active
       });
@@ -153,14 +153,15 @@ const Game = () => {
   }, [timeRemaining, showEndScreen]);
 
   const startGame = () => {
-    if(!webcamGranted || !isCameraActive())
-      {
-        alert("Please allow access to the camera and ensure it is active to start the game.");
+    if (!webcamGranted || !isCameraActive()) {
+      alert(
+        "Please allow access to the camera and ensure it is active to start the game."
+      );
       return;
-      } 
+    }
     newSessionId = uuidv4();
-    setSessionId(uuidv4()); // Generate a new session ID 
-    console.log("Session id generated..."+newSessionId)
+    setSessionId(uuidv4()); // Generate a new session ID
+    console.log("Session id generated..." + newSessionId);
     //handleUpload();
     setShuffledQuestions(questions.sort(() => Math.random() - 0.5));
     setCurrentQuestionIndex(0);
@@ -171,9 +172,9 @@ const Game = () => {
     setAnswerStates([]);
     setHasStarted(true); // Start the game
     captureIntervalRef.current = setInterval(captureImage, 10000);
-     // start image caputre(calling the image capture function for every 30 secs)
-    document.body.classList.remove('correct', 'wrong');
-    document.body.style.backgroundColor = ''; // Reset to original color at the start
+    // start image caputre(calling the image capture function for every 30 secs)
+    document.body.classList.remove("correct", "wrong");
+    document.body.style.backgroundColor = ""; // Reset to original color at the start
   };
 
   const captureImage = () => {
@@ -181,7 +182,8 @@ const Game = () => {
       alert("Camera is not active. Please ensure the camera is turned on.");
       return;
     }
-    if (!newSessionId) { // Check if sessionId is null
+    if (!newSessionId) {
+      // Check if sessionId is null
       console.error("Session ID is null. Cannot capture images.");
       return; // Skip capture if no session ID is set
     }
@@ -189,61 +191,74 @@ const Game = () => {
     // Capture the video feed
     const video = videoRef.current;
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    
+    const context = canvas.getContext("2d");
+
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
     // Capture the entire webpage using html2canvas
     html2canvas(document.body).then((screenshotCanvas) => {
-        screenshotCanvas.toBlob((blob) => {
-            const formData = new FormData();
-            formData.append('screenshot', blob, 'screenshot.png'); // Screenshot of the page
-            formData.append('newSessionId', newSessionId); // Add session ID to the form data
-            formData.append('sessionName', sessionName);
-            console.log("Captured screenshot. Session ID: " + newSessionId);
+      screenshotCanvas.toBlob((blob) => {
+        const formData = new FormData();
+        formData.append("screenshot", blob, "screenshot.png"); // Screenshot of the page
+        formData.append("newSessionId", newSessionId); // Add session ID to the form data
+        formData.append("sessionName", sessionName);
+        console.log("Captured screenshot. Session ID: " + newSessionId);
 
-            // Send the screenshot image to the server
-            axios.post('http://localhost:5000/screenshots', formData)
-              .then((response) => console.log('Screenshot uploaded:', response.data))
-              .catch((error) => console.error('Error uploading screenshot:', error));
-        });
+        // Send the screenshot image to the server
+        axios
+          .post("http://localhost:5000/screenshots", formData)
+          .then((response) =>
+            console.log("Screenshot uploaded:", response.data)
+          )
+          .catch((error) =>
+            console.error("Error uploading screenshot:", error)
+          );
+      });
     });
 
     // Capture the video feed image as before
     canvas.toBlob((blob) => {
       const formData = new FormData();
-      formData.append('image', blob, 'capture.png');  // Append the image file
-      formData.append('newSessionId', newSessionId);  // Append the session ID
-      formData.append('sessionName', sessionName);    // Append the session name
-    
-      console.log("Image captured. Session ID: " + newSessionId + " | Session Name: " + sessionName);
-    
+      formData.append("image", blob, "capture.png"); // Append the image file
+      formData.append("newSessionId", newSessionId); // Append the session ID
+      formData.append("sessionName", sessionName); // Append the session name
+
+      console.log(
+        "Image captured. Session ID: " +
+          newSessionId +
+          " | Session Name: " +
+          sessionName
+      );
+
       // Send the image, session ID, and session name to the server
-      axios.post('http://localhost:5000/uploads', formData)
-        .then((response) => console.log('Image uploaded with session details:', response.data))
-        .catch((error) => console.error('Error uploading image with session details:', error));
+      axios
+        .post("http://localhost:5000/uploads", formData)
+        .then((response) =>
+          console.log("Image uploaded with session details:", response.data)
+        )
+        .catch((error) =>
+          console.error("Error uploading image with session details:", error)
+        );
     });
-    
-};
-// const handleUpload = async () => {
-//   console.log("Handel upload called");
-//   const formData = new FormData();
-//   formData.append('newSessionId', newSessionId);  // Add sessionId
-//   formData.append('sessionName', sessionName); // Add sessionName
+  };
+  // const handleUpload = async () => {
+  //   console.log("Handel upload called");
+  //   const formData = new FormData();
+  //   formData.append('newSessionId', newSessionId);  // Add sessionId
+  //   formData.append('sessionName', sessionName); // Add sessionName
 
-//   try {
-//       const response = await fetch('http://localhost:5000', {
-//           method: 'POST',
-//           body: formData,
-//       });
+  //   try {
+  //       const response = await fetch('http://localhost:5000', {
+  //           method: 'POST',
+  //           body: formData,
+  //       });
 
-//       const result = await response.json();
-//       console.log(result);
-//   } catch (error) {
-//       console.error('Error uploading file:', error);
-//   }
-// };
-
+  //       const result = await response.json();
+  //       console.log(result);
+  //   } catch (error) {
+  //       console.error('Error uploading file:', error);
+  //   }
+  // };
 
   const setNextQuestion = () => {
     setSelectedAnswerIndex(null);
@@ -254,31 +269,35 @@ const Game = () => {
   const selectAnswer = (index, correct) => {
     if (selectedAnswerIndex !== null) return; // Prevent further clicks
 
-    const correctIndex = shuffledQuestions[currentQuestionIndex].answers.findIndex((ans) => ans.correct);
-    const newAnswerStates = shuffledQuestions[currentQuestionIndex].answers.map((ans, i) => {
-      if (i === correctIndex) {
-        return 'correct';
+    const correctIndex = shuffledQuestions[
+      currentQuestionIndex
+    ].answers.findIndex((ans) => ans.correct);
+    const newAnswerStates = shuffledQuestions[currentQuestionIndex].answers.map(
+      (ans, i) => {
+        if (i === correctIndex) {
+          return "correct";
+        }
+        if (i === index) {
+          return correct ? "correct" : "wrong";
+        }
+        return "wrong";
       }
-      if (i === index) {
-        return correct ? 'correct' : 'wrong';
-      }
-      return 'wrong';
-    });
+    );
 
     setSelectedAnswerIndex(index);
     setAnswerStates(newAnswerStates);
 
     if (correct) {
       setScore((prevScore) => prevScore + 1);
-      document.body.classList.add('correct');
+      document.body.classList.add("correct");
     } else {
-      document.body.classList.add('wrong');
+      document.body.classList.add("wrong");
     }
 
     // Delay for visual feedback before moving to the next question
     setTimeout(() => {
-      document.body.classList.remove('correct', 'wrong'); // Remove the class before next question
-      document.body.style.backgroundColor = ''; // Reset to original color
+      document.body.classList.remove("correct", "wrong"); // Remove the class before next question
+      document.body.style.backgroundColor = ""; // Reset to original color
 
       if (currentQuestionIndex + 1 < shuffledQuestions.length) {
         setNextQuestion();
@@ -291,15 +310,23 @@ const Game = () => {
   const endGame = () => {
     setShowEndScreen(true);
     clearInterval(captureIntervalRef.current); // Stop image capture
-    
-    document.body.classList.remove('correct', 'wrong');
-    document.body.style.backgroundColor = ''; // Reset to original color for end screen
+
+    document.body.classList.remove("correct", "wrong");
+    document.body.style.backgroundColor = ""; // Reset to original color for end screen
     triggerConfetti();
   };
 
   const triggerConfetti = () => {
     let particleCount = 500;
-    const colorOptions = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff", "#ffffff"];
+    const colorOptions = [
+      "#ff0000",
+      "#00ff00",
+      "#0000ff",
+      "#ffff00",
+      "#ff00ff",
+      "#00ffff",
+      "#ffffff",
+    ];
 
     const createConfetti = () => {
       confetti({
@@ -327,49 +354,86 @@ const Game = () => {
   return (
     <div className="game-container">
       {/* <Header /> */}
-      <video ref={videoRef} autoPlay playsInline style={{ display : 'none' }}></video>
-      <canvas ref={canvasRef} style={{ display: 'none' }} width="640" height="480"></canvas>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        style={{ display: "none" }}
+      ></video>
+      <canvas
+        ref={canvasRef}
+        style={{ display: "none" }}
+        width="640"
+        height="480"
+      ></canvas>
 
       {!webcamGranted ? (
         <div className="start-screen">
-          <button className="btn start-btn" onClick={requestWebcamAccess}>Allow to access camera</button>
+          <button className="btn start-btn" onClick={requestWebcamAccess}>
+            Allow to access camera
+          </button>
         </div>
-      ): !cameraActive ? (
+      ) : !cameraActive ? (
         <div className="start-screen">
-          <div className="message">Camera is not active. Please ensure your camera is turned on and try again.</div>
-          <button className="btn start-btn" onClick={requestWebcamAccess}>Retry Camera Access</button>
-        </div>)
-       :!hasStarted ? (
+          <div className="message">
+            Camera is not active. Please ensure your camera is turned on and try
+            again.
+          </div>
+          <button className="btn start-btn" onClick={requestWebcamAccess}>
+            Retry Camera Access
+          </button>
+        </div>
+      ) : !hasStarted ? (
         <div className="start-screen">
-          <button className="btn start-btn" onClick={startGame}>Start</button>
+          <button className="btn start-btn" onClick={startGame}>
+            Start
+          </button>
         </div>
       ) : !showEndScreen ? (
         <>
-          <div className="timer">Time Remaining: {Math.floor(timeRemaining / 60)}:{(timeRemaining % 60).toString().padStart(2, '0')}</div>
+          <div className="timer">
+            Time Remaining: {Math.floor(timeRemaining / 60)}:
+            {(timeRemaining % 60).toString().padStart(2, "0")}
+            <TimerBar />
+          </div>
           <div id="question-container">
-            <div className="question-text">{shuffledQuestions[currentQuestionIndex]?.question}</div>
-            <img src={shuffledQuestions[currentQuestionIndex]?.image} alt="Question" className="question-img" />
+            <div className="question-text">
+              {shuffledQuestions[currentQuestionIndex]?.question}
+            </div>
+            <img
+              src={shuffledQuestions[currentQuestionIndex]?.image}
+              alt="Question"
+              className="question-img"
+            />
             <div className="btn-grid">
-              {shuffledQuestions[currentQuestionIndex]?.answers.map((answer, index) => (
-                <button
-                  key={index}
-                  className={`btn ${answerStates[index] || ''}`}
-                  onClick={() => selectAnswer(index, answer.correct)}
-                  disabled={selectedAnswerIndex !== null}
-                >
-                  {answer.text}
-                </button>
-              ))}
+              {shuffledQuestions[currentQuestionIndex]?.answers.map(
+                (answer, index) => (
+                  <button
+                    key={index}
+                    className={`btn ${answerStates[index] || ""}`}
+                    onClick={() => selectAnswer(index, answer.correct)}
+                    disabled={selectedAnswerIndex !== null}
+                  >
+                    {answer.text}
+                  </button>
+                )
+              )}
             </div>
           </div>
         </>
       ) : (
         <div className="end-screen">
           <div className="end-message">
-            {score === questions.length ? "Congrats! You have scored full marks!" : "You’ve Completed the Quiz!"}
+            {score === questions.length
+              ? "Congrats! You have scored full marks!"
+              : "You’ve Completed the Quiz!"}
           </div>
-          <div className="score">Your score: {score}/{questions.length}</div>
-          <button className="btn" onClick={() => window.location.href = '/'}>Home</button>
+          <div className="score">
+            Your score: {score}/{questions.length}
+          </div>
+          <button className="btn" onClick={() => (window.location.href = "/")}>
+            Home
+          </button>
         </div>
       )}
     </div>
